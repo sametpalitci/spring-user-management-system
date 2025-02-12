16c038bf2b35cb1b52270b324841a2f41f11a1ef
Samet Palitci <abdulsametpalitci@gmail.com>
2025-02-12 21:12:02 +0300

    feat: add user and authentication controller

---
+++ src/main/java/com/sametpalitci/user_management_system/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.sametpalitci.user_management_system.controller;
+
+import com.sametpalitci.user_management_system.dto.AuthenticationRequest;
+import com.sametpalitci.user_management_system.dto.AuthenticationResponse;
+import com.sametpalitci.user_management_system.dto.RegisterRequest;
+import com.sametpalitci.user_management_system.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+} 
---
+++ src/main/java/com/sametpalitci/user_management_system/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.sametpalitci.user_management_system.controller;
+
+import com.sametpalitci.user_management_system.entity.User;
+import com.sametpalitci.user_management_system.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+} 
