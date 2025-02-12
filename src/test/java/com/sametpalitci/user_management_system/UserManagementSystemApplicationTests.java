a85b6f653cc0cbda96fdb0dc7380fb631d756808
Samet Palitci <abdulsametpalitci@gmail.com>
2025-02-12 20:27:28 +0300

    chore: initialize default spring project

---
+++ src/test/java/com/sametpalitci/user_management_system/UserManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametpalitci.user_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
